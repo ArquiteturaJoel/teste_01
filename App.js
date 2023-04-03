63c4d862427e899fbc29860325fa5bedf60d0e76
ArquiteturaJoel <arquitetura.joel@gmail.com>
2023-04-03 06:44:52 -0300

    mod 1

--- App.js
+++ App.js
@@ -5,17 +5,41 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <Text style={styles.title}>Open World</Text>
+      
+      <View style={styles.card}>
+        <Text style={styles.cardTitle}>React Native</Text>
+        <Text style={styles.cardSubtitle}>Create sample rs</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#2A2A2A',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fonte: 24,
+    marginBottom: 24
+  },
+  card: {
+    backgroundColor: "#FFF",
+    padding: 12,
+    borderRadius: 3
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  cardSubtitle:{
+    color: '999',
+    fontSize: 14
+  }
 });
